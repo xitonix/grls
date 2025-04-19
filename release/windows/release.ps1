dec142bdc6ed1c9eb9f3ad11d5dc134fea18c80a
Alex Golshani <xitonix.io@gmail.com>
2025-04-19 17:26:48 +1000

    win params

--- release/windows/release.ps1
+++ release/windows/release.ps1
@@ -1,11 +1,13 @@
 param($version,$binary)
 
+$version=($Env:GITHUB_REF).trimstart('refs/tags/v')
+
 If ( ! ( Test-Path Env:wix ) ) {
     Write-Error 'Could not find WiX binaries %wix%'
     exit 1
 }
 
-$msi="${binary}_${version}_amd64.msi"
+$msi="${Env:APPLICATION}_${version}_amd64.msi"
 $wixVersion="0.0.0"
 $wixVersionMatch=[regex]::Match($version, '^([0-9]+\.[0-9]+\.[0-9]+)')
 If ( $wixVersionMatch.success ) {
@@ -17,9 +19,9 @@ If ( $wixVersionMatch.success ) {
 
 .\build.ps1 `
   -version $version `
-  -binary $binary
+  -binary $Env:APPLICATION
 
-$appname=(Get-Culture).TextInfo.ToTitleCase($binary)
+$appname=(Get-Culture).TextInfo.ToTitleCase($Env:APPLICATION)
 
 & "${env:wix}bin\candle.exe" `
   -nologo `
